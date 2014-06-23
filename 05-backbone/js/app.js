41faa5e126944e7f767736b63552db8a1ff78198
Juan Pablo <juampi92@gmail.com>
2014-06-23 01:29:01 -0300

    Fixed remote images url

--- 05-backbone/js/app.js
+++ 05-backbone/js/app.js
@@ -30,7 +30,7 @@ function (Backbone,Movie,MovieView,FavsView){
   list.add( new Movie(
     { title:'Batman Begins',
       year:2005,
-      poster:'http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX640_SY720_.jpg',
+      poster:'http://upload.wikimedia.org/wikipedia/en/a/af/Batman_Begins_Poster.jpg',
       description: 'After training with his mentor, Batman begins his war on crime to free the crime-ridden Gotham City from corruption that the Scarecrow and the League of Shadows have cast upon it.',
       cast: [
         { actor:'Christian Bale', role:'Bruce Wayne' },
@@ -45,7 +45,7 @@ function (Backbone,Movie,MovieView,FavsView){
   list.add( new Movie(
     { title:'The Dark Knight',
       year:2008,
-      poster:'http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX640_SY720_.jpg',
+      poster:'http://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg',
       description: 'When Batman, Gordon and Harvey Dent launch an assault on the mob, they let the clown out of the box, the Joker, bent on turning Gotham on itself and bringing any heroes down to his level.',
       cast: [
         { actor:'Christian Bale', role:'Bruce Wayne' },
@@ -59,7 +59,7 @@ function (Backbone,Movie,MovieView,FavsView){
   list.add( new Movie(
     { title:'The Dark Knight Rises',
       year:2012,
-      poster:'http://ia.media-imdb.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_SX640_SY720_.jpg',
+      poster:'http://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg',
       description: 'Eight years after the Joker`s reign of anarchy, the Dark Knight must return to defend Gotham City against the enigmatic jewel thief Catwoman and the ruthless mercenary Bane as the city teeters on the brink of complete annihilation.',
       cast: [
         { actor:'Christian Bale', role:'Bruce Wayne' },
